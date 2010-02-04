1b86c2e3ae902d39be49cb489b2f4d6b2a59d6ee
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-03 19:27:33 -0800

    Improved mylibrary.rb, added more variants of statements whose path coverage is and isn't shown, and annotated these.

--- mylibrary/lib/mylibrary.rb
+++ mylibrary/lib/mylibrary.rb
@@ -18,7 +18,19 @@ class MyLibrary
   def run
     hello
 
+    # No branch coverage shown for these
     mypet = lucky? ? get_kitten : get_hyena
+
+    mypet = lucky? && get_kitten || get_hyena
+
+    mypet = lucky? &&
+      get_kitten ||
+      get_hyena
+
+    # Branch coverage is shown for these
+    mypet = lucky? ?
+      get_kitten :
+      get_hyena
 
     mypet = \
       if lucky?
@@ -27,8 +39,10 @@ class MyLibrary
         get_hyena
       end
 
+    # No branch coverage shown
     puts 1/0 unless lucky?
 
+    # Branch coverage is shown
     unless lucky?
       puts 1/0
     end
